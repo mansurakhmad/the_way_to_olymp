c8a8ae0ba337e42860d4b04c1b3d9931365ca146
mansurakhmad <mansurakhmad.051294@gmail.com>
2026-01-01 21:57:55 +0300

    init: added prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from '@/app/App.vue';
+import PrimeVue from 'primevue/config';
 import { router } from '@/app/router';
-
+import Aura from '@primeuix/themes/aura';
 import '@/app/styles/index.css';
 
 const app = createApp(App);
+
+app.use(PrimeVue, { theme: { preset: Aura } });
 app.use(router);
 app.mount('#app');
